200cc7d62a03b11de798653e14c0e5687371f784
Juliano Marangoni <113652466+Z44CK@users.noreply.github.com>
2023-08-22 22:47:37 -0300

    Update and rename 003 to 003.py

--- 003.py
+++ 003.py
@@ -1,4 +1,4 @@
-Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
+# Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
 
 data = input('Data:dd/mm/aaaa: ')
 
