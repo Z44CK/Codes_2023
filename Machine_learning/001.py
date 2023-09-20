1c055d879e6ffee7291dd2a1022439be499643ab
Juliano Marangoni <113652466+Z44CK@users.noreply.github.com>
2023-09-19 23:39:27 -0300

    Update 001.py

--- Machine_learning/001.py
+++ Machine_learning/001.py
@@ -1,68 +1,83 @@
-import numpy as np
-from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, cross_val_predict
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from keras.datasets import mnist
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.neural_network import MLPClassifier
+from scipy.stats import uniform, randint
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import reciprocal
 
-# Carregar o conjunto de dados MNIST
-mnist = fetch_openml("mnist_784", version=1)
-X, y = mnist.data, mnist.target.astype(int)
 
-# Dividir o conjunto de dados em treinamento (80%) e teste (20%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# carregando o dados do mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Padronizar os dados
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# Redimensionando as imagens para um vetor unidimensional
+x_train = x_train.reshape(x_train.shape[0], -1)
 
-# Definir a grade de hiperparâmetros para pesquisa aleatória
+# Normalizando os valores de pixel para estar no intervalo [0, 1]
+x_train = x_train.astype('float32') / 255
+
+# Definindo o modelo MLP
+model = MLPClassifier()
+
+# Definindo a grade de hiperparâmetros para a busca aleatória
 param_dist = {
-    'C': reciprocal(0.001, 10),
-    'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
-    'gamma': reciprocal(0.001, 0.1),
+    'hidden_layer_sizes': [(64,), (128,), (64, 64), (128, 64)],
+    'alpha': uniform(0.0001, 0.1),
+    'activation': ['logistic', 'tanh', 'relu'],
+    'learning_rate': ['constant', 'invscaling', 'adaptive'],
+    'learning_rate_init': uniform(0.001, 0.1),
+    'max_iter': randint(100, 1000),
 }
+# Inicializando a busca aleatória com validação cruzada
+random_search = RandomizedSearchCV(model, param_distributions=param_dist, n_iter=10, cv=5, random_state=42, n_jobs=-1,
+                                   verbose=2)
 
-# Criar um classificador SVM
-clf = SVC()
+# Realizando a busca aleatória
+random_search.fit(x_train, y_train)
 
-# Realizar a pesquisa aleatória com validação cruzada (k = 5)
-random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=10, cv=5, n_jobs=-1)
-random_search.fit(X_train, y_train)
-
-# Obter os melhores hiperparâmetros
+# Melhores parâmetros encontrados pela busca aleatória
 best_params = random_search.best_params_
+print("Melhores Parâmetros Encontrados:", best_params)
 
-# Imprimir os melhores hiperparâmetros
-print("Melhores Hiperparâmetros Encontrados:")
-print(best_params)
+# Treinando o modelo com os melhores parâmetros
+best_model = MLPClassifier(**best_params)
+best_model.fit(x_train, y_train)
 
-# Treinar o modelo com os melhores hiperparâmetros no conjunto de treinamento completo
-best_model = SVC(**best_params)
-best_model.fit(X_train, y_train)
+# Avaliando o modelo no conjunto de teste
+x_test = x_test.reshape(x_test.shape[0], -1)
+x_test = x_test.astype('float32') / 255
+accuracy = best_model.score(x_test, y_test)
+print(f'Acurácia no conjunto de teste: {accuracy * 100:.2f}%')
 
-# Fazer previsões no conjunto de teste usando validação cruzada (k = 5)
-y_pred = cross_val_predict(best_model, X_test, y_test, cv=5, n_jobs=-1)
+# numeros de folds(k)
+k = 5
+# inicializa o objeto k-fold
+kf = KFold(n_splits=k, shuffle=True, random_state=42)
 
-# Calcular a acurácia
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Acurácia no conjunto de teste: {accuracy:.4f}')
+# dividindo o conjunto de treinamentos em k folds
+for fold, (train_indices, val_indices) in enumerate(kf.split(x_train)):
+    x_train_fold, x_val_fold = x_train[train_indices], x_train[val_indices]
+    y_train_fold, y_val_fold = y_train[train_indices], y_train[val_indices]
 
-# Exibir o relatório de classificação
-report = classification_report(y_test, y_pred)
-print("Relatório de Classificação:\n", report)
+    print(f'Dobra {fold + 1} - Tamanho do conjunto de treinamento: {len(x_train_fold)} amostras')
+    print(f'Dobra {fold + 1} - Tamanho do conjunto de validação: {len(x_val_fold)} amostras')
 
-# Criar a matriz de confusão
-confusion = confusion_matrix(y_test, y_pred)
 
-# Exibir a matriz de confusão usando o seaborn
+# divindo o treinamento e teste em 80/20
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+
+print(f"Tamanho do conjunto de treinamento: {len(x_train)} amostras")
+print(f"Tamanho do conjunto de validação: {len(x_val)} amostras")
+print(f"Tamanho do conjunto de teste: {len(x_test)} amostras")
+
+y_pred = best_model.predict(x_test)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
 plt.figure(figsize=(10, 8))
-sns.heatmap(confusion, annot=True, fmt='d', cmap='Blues', cbar=False)
-plt.xlabel('Previsto')
-plt.ylabel('Verdadeiro')
+sns.heatmap(conf_matrix, annot=True, fmt='d', camp='Blues', cbar=False)
+plt.xlabel('Previsão')
+plt.ylabel('Valor Real')
 plt.title('Matriz de Confusão')
 plt.show()
