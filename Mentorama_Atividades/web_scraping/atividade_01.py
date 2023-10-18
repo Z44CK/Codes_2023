175d95206dc9542f66a288bdf61990d449578e56
Juliano Marangoni <113652466+Z44CK@users.noreply.github.com>
2023-10-17 22:55:54 -0300

    Update atividade_01.py

--- Mentorama_Atividades/web_scraping/atividade_01.py
+++ Mentorama_Atividades/web_scraping/atividade_01.py
@@ -1,3 +1,10 @@
+# Desenvolva um Web Crawler para “navegar” pelas páginas da Wikipédia:
+
+# - Escolha uma página da Wikipédia e realize a raspagem dos dados da página.
+# - Imprima na tela a mensagem “Página principal: {Nome da página visitada}”
+# - Busque no texto da página em que foi feita a raspagem todos os links que apontem para outras páginas da Wikipédia.
+# - Faça uma nova raspagem para cada novo link capturado e imprima em cada um deles a mensagem “Página secundária: {Nome da página visitada}”.
+
 import requests
 from bs4 import BeautifulSoup
 import re
